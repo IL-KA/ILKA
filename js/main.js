205563d8c3becf99e68d58e287782ab122367522
IL-KA <ikart@bk.ru>
2023-07-01 19:12:31 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+const navList = document.getElementsByClassName('nav-item');
+// const body = document.querySelector('#body')
+
+navBtn.onclick = () => {
+  if (nav.classList.toggle('open')) {
+    navBtnImg.src = "./img/icons/nav-close.svg"
+    // body.overflow = onpagehide();
+  } else {
+    navBtnImg.src = "./img/icons/nav-open.svg"
+  }
+}
+
+if (window.innerWidth <= 1230) {
+  for (let i = 0; i < navList.length; i++) {
+      navList[i].setAttribute('data-aos', 'fade-left');
+  }
+} else {
+  for (let i = 0; i < navList.length; i++) {
+    navList[i].onclick = () => {
+      nav.classList.remove('open');
+      navBtnImg.src = "./img/icons/nav-open.svg";
+    };
+  }
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Скролл к якорной ссылке
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
