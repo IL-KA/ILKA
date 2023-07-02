181f2e0c104aea7c8ec152d23a2f2aaa1ab37d3a
IL-KA <ikart@bk.ru>
2023-07-02 16:20:51 +0300

    + close button in nav

--- js/main.js
+++ js/main.js
@@ -4,29 +4,20 @@ const navBtnImg = document.querySelector('#nav-btn-img');
 const navList = document.getElementsByClassName('nav-link');
 const body = document.querySelector('#body')
 
+if (window.innerHeight <= 500) {
+  AOS.init({
+    once:true
+  });
+} else {
+  AOS.init();
+}
+
 navBtn.onclick = () => {
   if (nav.classList.toggle('open')) {
-    navBtnImg.hidden = true;
-    // navBtnImg.src = "./img/icons/nav-close.svg"
     body.classList.add('no-scroll')
-
-    // var listenScroll = true;
-  //   function scroller(ancor) {
-  //     $(window).scroll(function(event) {
-  //       if (ancor!==-1 && listenScroll === true) {
-  //         window.scrollTo({
-  //           top: ancor,
-  //           behavior: "instant"
-  //         });
-  //       }	else {
-  //         return false
-  //       }
-  //     });
-  //   }
   }
   else {
-    navBtnImg.src = "./img/icons/nav-open.svg"
-    navBtnImg.hidden = false;
+    nav.classList.remove('open');
     body.classList.remove('no-scroll')
   }
 }
@@ -36,18 +27,16 @@ document.addEventListener( 'click', (e) => {
   if ( ! withinBoundaries ) {
     nav.classList.remove('open');
     body.classList.remove('no-scroll')
-    navBtnImg.hidden = false;
-    navBtnImg.src = "./img/icons/nav-open.svg"
   }
 })
+
+
 
 if (window.innerWidth <= 1230) {
   for (let i = 0; i < navList.length; i++) {
     navList[i].onclick = () => {
       nav.classList.remove('open');
       body.classList.remove('no-scroll')
-      navBtnImg.hidden = false;
-      navBtnImg.src = "./img/icons/nav-open.svg";
     };
   }
 }
@@ -55,7 +44,6 @@ if (window.innerWidth <= 1230) {
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-
     // Скролл к якорной ссылке
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
